71ba84fd84fe81161771ee84483da379dd1ea17e
mosaxiv <mosaxiv@gmail.com>
2017-12-15 09:26:57 +0900

    add test

---
+++ tests/TestCase/View/Helper/SecureTargetBlankHelperTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SecureTargetBlank\Test\TestCase\View\Helper;
+
+use Cake\TestSuite\TestCase;
+
+class SecureTargetBlankHelperTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testLink()
+    {
+        $this->assertTrue(true);
+    }
+}
+
+
