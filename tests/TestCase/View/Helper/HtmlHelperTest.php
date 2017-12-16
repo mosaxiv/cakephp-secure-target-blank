68be63f9deefe3dd43105057171a581eb4b459fa
mosaxiv <mosaxiv@gmail.com>
2017-12-16 15:03:36 +0900

    add testLink()

--- tests/TestCase/View/Helper/HtmlHelperTest.php
+++ tests/TestCase/View/Helper/HtmlHelperTest.php
@@ -2,13 +2,25 @@
 
 namespace SecureTargetBlank\Test\TestCase\View\Helper;
 
+use Cake\Routing\Router;
 use Cake\TestSuite\TestCase;
+use Cake\View\View;
+use SecureTargetBlank\View\Helper\HtmlHelper;
 
 class HtmlHelperTest extends TestCase
 {
+    /**
+     * Instance of HtmlHelper.
+     *
+     * @var HtmlHelper
+     */
+    public $Html;
+
     public function setUp()
     {
         parent::setUp();
+        Router::connect('/:controller/:action/*');
+        $this->Html = new HtmlHelper(new View());
     }
 
     public function tearDown()
@@ -18,6 +30,28 @@ class HtmlHelperTest extends TestCase
 
     public function testLink()
     {
-        $this->assertTrue(true);
+        $expected = $this->Html->link('test_title', ['controller' => 'test']);
+        $actual = '<a href="/test/index">test_title</a>';
+        $this->assertEquals($expected, $actual);
+
+        $expected = $this->Html->link('test_title', ['controller' => 'test'], ['target' => '_blank']);
+        $actual = '<a href="/test/index" target="_blank" rel="noopener noreferrer">test_title</a>';
+        $this->assertEquals($expected, $actual);
+
+        $expected = $this->Html->link('test_title', ['controller' => 'test'], ['target' => '_blank', 'rel' => 'noopener']);
+        $actual = '<a href="/test/index" target="_blank" rel="noopener noreferrer">test_title</a>';
+        $this->assertEquals($expected, $actual);
+
+        $expected = $this->Html->link('test_title', ['controller' => 'test'], ['target' => '_blank', 'rel' => 'nofollow']);
+        $actual = '<a href="/test/index" target="_blank" rel="nofollow noopener noreferrer">test_title</a>';
+        $this->assertEquals($expected, $actual);
+
+        $expected = $this->Html->link('test_title', ['controller' => 'test'], ['target' => '_blank', 'rel' => 'nofollow test']);
+        $actual = '<a href="/test/index" target="_blank" rel="nofollow test noopener noreferrer">test_title</a>';
+        $this->assertEquals($expected, $actual);
+
+        $expected = $this->Html->link('test_title', ['controller' => 'test'], ['target' => '_blank', 'rel' => ['nofollow', 'test']]);
+        $actual = '<a href="/test/index" target="_blank" rel="nofollow test noopener noreferrer">test_title</a>';
+        $this->assertEquals($expected, $actual);
     }
 }
