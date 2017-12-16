9450d0927a2b3231e339d83490fc0913e91b5d24
mosaxiv <mosaxiv@gmail.com>
2017-12-16 15:02:22 +0900

    add 'noopener'/'noreferrer'

--- src/View/Helper/HtmlHelper.php
+++ src/View/Helper/HtmlHelper.php
@@ -12,6 +12,15 @@ class HtmlHelper extends \Cake\View\Helper\HtmlHelper
      */
     public function link($title, $url = null, array $options = [])
     {
-        parent::link($title, $url, $options);
+        if (isset($options['target']) && $options['target'] === '_blank') {
+            $options['rel'] = $options['rel'] ?? [];
+            if (is_string($options['rel'])) {
+                $options['rel'] = explode(' ', $options['rel']);
+            }
+            $options['rel'] = array_merge($options['rel'], ['noopener', 'noreferrer']);
+            $options['rel'] = array_unique($options['rel']);
+        }
+
+        return parent::link($title, $url, $options);
     }
 }
